ae910f7a8939459605c5457296e4c08fa4f80d7e
Courtney Robinson <courtney.robinson@hypi.io>
2021-03-19 16:11:22 +0000

    Update sidebars.js

--- sidebars.js
+++ sidebars.js
@@ -1,7 +1,7 @@
 module.exports = {
   someSidebar: {
     'Getting Started': ['introduction', 'overview'],
-    'API References': ['authentication','hypiobject','createdata','updatedata','readdata','deletedata','crud','authorization','mathsapi','aggregation'],
+    'API References': ['authentication','hypiobject','createdata','updatedata','readdata','deletedata','crud','authorisation','mathsapi','aggregation'],
     'Sample App': ['mdx'],
     'User Interface': ['mdx'],
   },
