cc107d7c2413c45d2028c4b1a6a6680224acd79f
SAIASA\DELL <asawari.pachkhede@gmail.com>
2022-02-15 17:58:59 +0530

    New config for Tink

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -61,23 +61,23 @@ module.exports = {
             },
             {
               label: 'Get Started',
-              to: 'docs/get-started/',
+              to: 'docs/lowcode/get-started/',
             },
             {
               label: 'Authentication',
-              to: 'docs/authentication',
+              to: 'docs/lowcode/authentication',
             },
             {
               label: 'Filtering your data',
-              to: 'docs/arcql',
+              to: 'docs/lowcode/arcql',
             },
             {
               label: 'GraphQL CRUD APIs',
-              to: 'docs/crud',
+              to: 'docs/lowcode/crud',
             },
             {
               label: 'REST APIs',
-              to: 'docs/rest-apis',
+              to: 'docs/lowcode/rest-apis',
             },
           ],
         },
@@ -292,7 +292,366 @@ module.exports = {
             to: '/docs/api-references',
             from: ['/references/'],
           },
-          
+          {
+            to: '/docs/lowcode/', 
+            from: ['/docs/'], 
+          },
+          {
+            to: '/docs/lowcode/overview', 
+            from: ['/docs/overview'], 
+          },
+          {
+            to: '/docs/lowcode/get-started', 
+            from: ['/docs/get-started'], 
+          },
+          {
+            to: '/docs/lowcode/faq', 
+            from: ['/docs/faq'], 
+          },
+          {
+            to: '/docs/lowcode/api-references', 
+            from: ['/docs/api-references'], 
+          },
+          {
+            to: '/docs/lowcode/apisetup', 
+            from: ['/docs/apisetup'], 
+          },
+          {
+            to: '/docs/lowcode/authentication', 
+            from: ['/docs/authentication'], 
+          },
+          {
+            to: '/docs/lowcode/scalars', 
+            from: ['/docs/scalars'], 
+          },
+          {
+            to: '/docs/lowcode/magic-hypi-object', 
+            from: ['/docs/magic-hypi-object'], 
+          },
+          {
+            to: '/docs/lowcode/crud', 
+            from: ['/docs/crud'], 
+          },
+          {
+            to: '/docs/lowcode/create-data', 
+            from: ['/docs/create-data'], 
+          },
+          {
+            to: '/docs/lowcode/update-data', 
+            from: ['/docs/update-data'], 
+          },
+          {
+            to: '/docs/lowcode/read-data', 
+            from: ['/docs/read-data'], 
+          },
+          {
+            to: '/docs/lowcode/delete-data', 
+            from: ['/docs/delete-data'], 
+          },
+          {
+            to: '/docs/lowcode/arcql', 
+            from: ['/docs/arcql'], 
+          },
+          {
+            to: '/docs/lowcode/references', 
+            from: ['/docs/references'], 
+          },
+          {
+            to: '/docs/lowcode/authorisation', 
+            from: ['/docs/authorisation'], 
+          },
+          {
+            to: '/docs/lowcode/realtime-db', 
+            from: ['/docs/realtime-db'], 
+          },
+          {
+            to: '/docs/lowcode/apigateway', 
+            from: ['/docs/apigateway'], 
+          },
+          {
+            to: '/docs/lowcode/aggregation', 
+            from: ['/docs/aggregation'], 
+          },
+          {
+            to: '/docs/lowcode/maths-api', 
+            from: ['/docs/maths-api'], 
+          },
+          {
+            to: '/docs/lowcode/hypi-directives', 
+            from: ['/docs/hypi-directives'], 
+          },
+	  {
+            to: '/docs/lowcode/user-defined-function', 
+            from: ['/docs/user-defined-function'], 
+          },
+          {
+            to: '/docs/lowcode/workflow', 
+            from: ['/docs/workflow'], 
+          },
+	  {
+            to: '/docs/lowcode/triggers', 
+            from: ['/docs/triggers'], 
+          },
+	  {
+            to: '/docs/lowcode/webhooks', 
+            from: ['/docs/webhooks'], 
+          },
+          {
+            to: '/docs/lowcode/fileupload', 
+            from: ['/docs/fileupload'], 
+          },
+          {
+            to: '/docs/lowcode/rest-apis', 
+            from: ['/docs/rest-apis'], 
+          },
+          {
+            to: '/docs/lowcode/oauth2login', 
+            from: ['/docs/oauth2login'], 
+          },
+          {
+            to: '/docs/lowcode/password-reminder', 
+            from: ['/docs/password-reminder'], 
+          },
+          {
+            to: '/docs/lowcode/serverless', 
+            from: ['/docs/serverless'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-cli', 
+            from: ['/docs/openwhisk-cli'], 
+          },
+          {
+            to: '/docs/lowcode/create-serverless', 
+            from: ['/docs/create-serverless'], 
+          },
+	  {
+            to: '/docs/lowcode/use-serverless', 
+            from: ['/docs/use-serverless'], 
+          },
+	  {
+            to: '/docs/lowcode/openwhisk-actions', 
+            from: ['/docs/openwhisk-actions'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-parameters', 
+            from: ['/docs/openwhisk-parameters'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-annotations', 
+            from: ['/docs/openwhisk-annotations'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-triggers', 
+            from: ['/docs/openwhisk-triggers'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-sequences', 
+            from: ['/docs/openwhisk-sequences'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-conductors', 
+            from: ['/docs/openwhisk-conductors'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-packages', 
+            from: ['/docs/openwhisk-packages'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-webactions', 
+            from: ['/docs/openwhisk-webactions'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-alarms', 
+            from: ['/docs/openwhisk-alarms'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-slack', 
+            from: ['/docs/openwhisk-slack'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-websocket', 
+            from: ['/docs/openwhisk-websocket'], 
+          },
+          {
+            to: '/docs/lowcode/openwhisk-github', 
+            from: ['/docs/openwhisk-github'], 
+          },
+          {
+            to: '/docs/lowcode/actions-nodejs', 
+            from: ['/docs/actions-nodejs'], 
+          },
+          {
+            to: '/docs/lowcode/actions-python', 
+            from: ['/docs/actions-python'], 
+          },
+          {
+            to: '/docs/lowcode/actions-java', 
+            from: ['/docs/actions-java'], 
+          },
+          {
+            to: '/docs/lowcode/actions-swift', 
+            from: ['/docs/actions-swift'], 
+          },
+          {
+            to: '/docs/lowcode/actions-go', 
+            from: ['/docs/actions-go'], 
+          },
+          {
+            to: '/docs/lowcode/actions-php', 
+            from: ['/docs/actions-php'], 
+          },
+          {
+            to: '/docs/lowcode/actions-ruby', 
+            from: ['/docs/actions-ruby'], 
+          },
+          {
+            to: '/docs/lowcode/actions-rust', 
+            from: ['/docs/actions-rust'], 
+          },
+          {
+            to: '/docs/lowcode/actions-dotnet', 
+            from: ['/docs/actions-dotnet'], 
+          },
+          {
+            to: '/docs/lowcode/actions-docker', 
+            from: ['/docs/actions-docker'], 
+          },
+          {
+            to: '/docs/lowcode/hypi-cli-intro', 
+            from: ['/docs/hypi-cli-intro'], 
+          },
+          {
+            to: '/docs/lowcode/hypi-cli-flutter', 
+            from: ['/docs/hypi-cli-flutter'], 
+          },
+          {
+            to: '/docs/lowcode/hypi-cli-react', 
+            from: ['/docs/hypi-cli-react'], 
+          },
+          {
+            to: '/docs/lowcode/hypi-cli-angular', 
+            from: ['/docs/hypi-cli-angular'], 
+          },
+          {
+            to: '/docs/lowcode/hypi-cli-vuejs-composition', 
+            from: ['/docs/hypi-cli-vuejs-composition'], 
+          },
+          {
+            to: '/docs/lowcode/hypi-cli-vuejs-smartqueries', 
+            from: ['/docs/hypi-cli-vuejs-smartqueries'], 
+          },
+          {
+            to: '/docs/lowcode/graphql', 
+            from: ['/docs/graphql'], 
+          },
+          {
+            to: '/docs/lowcode/gql-introduction', 
+            from: ['/docs/gql-introduction'], 
+          },
+          {
+            to: '/docs/lowcode/gql-schema-types', 
+            from: ['/docs/gql-schema-types'], 
+          },
+          {
+            to: '/docs/lowcode/gql-queries', 
+            from: ['/docs/gql-queries'], 
+          },
+          {
+            to: '/docs/lowcode/gql-mutations', 
+            from: ['/docs/gql-mutations'], 
+          },
+          {
+            to: '/docs/lowcode/gql-interfaces', 
+            from: ['/docs/gql-interfaces'], 
+          },
+          {
+            to: '/docs/lowcode/core', 
+            from: ['/docs/core'], 
+          },
+          {
+            to: '/docs/lowcode/react-todo-app', 
+            from: ['/docs/react-todo-app'], 
+          },
+          {
+            to: '/docs/lowcode/hypi-angular', 
+            from: ['/docs/hypi-angular'], 
+          },
+          {
+            to: '/docs/lowcode/tutorials', 
+            from: ['/docs/tutorials'], 
+          },
+          {
+            to: '/docs/lowcode/gql-crud-tutorial', 
+            from: ['/docs/gql-crud-tutorial'], 
+          },
+          {
+            to: '/docs/lowcode/time-series-aggregations', 
+            from: ['/docs/time-series-aggregations'], 
+          },
+          {
+            to: '/docs/lowcode/ui-create-app', 
+            from: ['/docs/ui-create-app'], 
+          },
+          {
+            to: '/docs/lowcode/ui-create-release', 
+            from: ['/docs/ui-create-release'], 
+          },
+          {
+            to: '/docs/lowcode/ui-create-table-vb', 
+            from: ['/docs/ui-create-table-vb'], 
+          },
+          {
+            to: '/docs/lowcode/ui-create-table-gse', 
+            from: ['/docs/ui-create-table-gse'], 
+          },
+          {
+            to: '/docs/lowcode/ui-add-environment-var', 
+            from: ['/docs/ui-add-environment-var'], 
+          },
+          {
+            to: '/docs/lowcode/ui-add-dependencies', 
+            from: ['/docs/ui-add-dependencies'], 
+          },
+          {
+            to: '/docs/lowcode/ui-deprecate-publish', 
+            from: ['/docs/ui-deprecate-publish'], 
+          },
+          {
+            to: '/docs/lowcode/ui-create-instance', 
+            from: ['/docs/ui-create-instance'], 
+          },
+          {
+            to: '/docs/lowcode/ui-view-instance', 
+            from: ['/docs/ui-view-instance'], 
+          },
+          {
+            to: '/docs/lowcode/ui-instance-api-config', 
+            from: ['/docs/ui-instance-api-config'], 
+          },
+          {
+            to: '/docs/lowcode/ui-gql-config', 
+            from: ['/docs/ui-gql-config'], 
+          },
+          {
+            to: '/docs/lowcode/ui-gql-playground', 
+            from: ['/docs/ui-gql-playground'], 
+          },
+          {
+            to: '/docs/lowcode/ui-data-migration', 
+            from: ['/docs/ui-data-migration'], 
+          },
+          {
+            to: '/docs/lowcode/ui-real-time-streams', 
+            from: ['/docs/ui-real-time-streams'], 
+          },
+          {
+            to: '/docs/lowcode/glossary', 
+            from: ['/docs/glossary'], 
+          },
+          {
+            to: '/docs/lowcode/get-involved', 
+            from: ['/docs/get-involved'], 
+          }          
         ],
       },
     ],
--- sidebars.js
+++ sidebars.js
@@ -1,47 +1,92 @@
 module.exports = {
   docs: [
-    {
+      {
+       type: 'category',
+       label: 'Welcome',
+       items: ['hypi-tink-intro', 'hypi-tink-overview', 'hypi-tink-get-started'],
+      },
+      {
+       type: 'category',
+       label: 'Mood Board',
+       items: ['hypi-tink-mood'],
+      },
+      {
+       type: 'category',
+       label: 'Stories Board',
+       items: ['hypi-tink-stories'],
+      },
+      {
+       type: 'category',
+       label: 'Stories Board',
+       items: ['hypi-tink-stories','stories-users','stories-action','stories-outcome'],
+      },
+      {
+       type: 'category',
+       label: 'Data Tables',
+       items: ['stories-data-tables','data-tables','data-tables-versions','data-tables-environment','data-console'],
+      },
+      {
+       type: 'category',
+       label: 'Designer Board',
+       items: ['hypi-tink-designer','designer-screens','designer-components','designer-widgets','designer-assets'],
+      },
+      {
+       type: 'category',
+       label: 'Widget',
+       items: ['widget-groups','widget-basic','widget-layout','widget-forms','widget-advance','widget-list-view'],
+      },
+      {
+       type: 'category',
+       label: 'Logic',
+       items: ['designer-logic','logic-output-binding','logic-input-binding'],
+      },
+      'hypi-tink-glossary',
+     {
       type: 'category',
-      label: 'Welcome',
+      label: 'Lowcode',
+      items: [
+     {
+      type: 'category',
+      label: 'Hypi's Low Code Backend',
       items: ['introduction', 'overview', 'get-started','faq'],
-    },
-    {
+     },
+     {
       type: 'category',
       label: 'API & References',
       items: ['api-references','apisetup','authentication','scalars','hypiobject','crud','createdata','updatedata','readdata','deletedata','arcql','references','authorisation','realtime-db','apigateway','aggregation','mathsapi','hypi-directives','userdefinedfunctions','workflow','triggers','webhooks','fileupload','rest-apis','oauth2login','password-reminder'],
-    },
-    {
+     },
+     {
       type: 'category',
       label: 'Serverless',
       items: ['serverless', 'openwhisk-cli', 'create-serverless','use-serverless','openwhisk-actions','openwhisk-parameters','openwhisk-annotations','openwhisk-triggers','openwhisk-sequences','openwhisk-conductors','openwhisk-packages','openwhisk-webactions','openwhisk-alarms','openwhisk-slack','openwhisk-websocket','openwhisk-github'],
-    },
-    {
+     },
+     {
       type: 'category',
       label: 'Serverless: Language Support',
       items: ['actions-nodejs','actions-python','actions-java','actions-swift','actions-go','actions-php','actions-ruby','actions-rust','actions-dotnet','actions-docker'],
-    },
-    {
+     },
+     {
       type: 'category',
       label: 'Hypi CLI',
       items: ['hypi-cli-intro','hypi-cli-flutter','hypi-cli-react','hypi-cli-angular','hypi-cli-vuejs-composition','hypi-cli-vuejs-smartqueries'],
-    },
-    {
+     },
+     {
       type: 'category',
       label: 'GraphQL',
       items: ['graphql', 'gql-introduction', 'gql-schema-types','gql-queries','gql-mutations','gql-interfaces'],
-    },
-    {
+     },
+     {
       type: 'category',
       label: 'System Apps',
       items: ['core'],
-    },
-    {
+     },
+     {
       type: 'category',
       label: 'Example Apps',
       items: ['react-todo-app'],
-    },
-    'hypi-angular',
-    {
+     },
+     'hypi-angular',
+     {
       type: 'category',
       label: 'Tutorial',
       items: [
@@ -49,18 +94,20 @@ module.exports = {
 	'gql-crud-tutorial',
 	'time-series-aggregations',
 	], 
-    },
-    {
+     },
+     {
       type: 'category',
       label: 'User Interface Guide',
       items: ['ui-create-app','ui-create-release','ui-create-table-vb','ui-create-table-gse','ui-add-environment-var','ui-add-dependencies','ui-deprecate-publish','ui-create-instance','ui-view-instance','ui-instance-api-config','ui-gql-config','ui-gql-playground','ui-data-migration','ui-real-time-streams'],
-    },
-    'glossary',
-    'get-involved',
-   {
+     },
+     'glossary',
+     'get-involved',
+    {
       type: 'category',
       label: 'Release Notes',
       items: [],
-    },
+     },
+    ],    
+   },
   ],
 };
